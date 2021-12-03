1a7353d8a952a617829ea72b90ad5a669064935f
Joy Chandra Mollik <xoycmollik913@gmail.com>
2021-12-03 16:29:46 +0600

    home changed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Home = () => {
 	return (
 		<div>
-			<h4>This is home</h4>
+			<h1>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
+				minima eum aut veritatis maxime in iure aliquid quo reiciendis,
+				vel expedita nulla dolor sequi nam autem voluptate rem quisquam
+				laudantium. Corporis tempore placeat doloribus ipsam delectus
+				magni animi perferendis recusandae beatae iste blanditiis eos,
+				molestias natus, suscipit porro ducimus officia nemo quaerat
+				quia laboriosam quod ex expedita dolores? Itaque eos nam aliquam
+				laborum harum eius veritatis ratione minima fugit. Nulla laborum
+				impedit quae alias quod pariatur sunt recusandae ex? Perferendis
+				corrupti labore, quas illo explicabo ducimus quidem ad minima a
+				laborum voluptatibus? Doloribus porro quod exercitationem
+				voluptatibus saepe quam omnis!
+			</h1>
 		</div>
 	);
 };
