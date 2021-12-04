2adc75e85a7ff15a9bd3edb53c7bb36324163229
Shahida Mahtab <marwa.mahtab@gmail.com>
2021-12-04 16:21:27 +0600

    marwa's conflict

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-	return (
-		<div>
-			<h1>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad
-				minima eum aut veritatis maxime in iure aliquid quo reiciendis,
-				vel expedita nulla dolor sequi nam autem voluptate rem quisquam
-				laudantium. Corporis tempore placeat doloribus ipsam delectus
-				magni animi perferendis recusandae beatae iste blanditiis eos,
-				molestias natus, suscipit porro ducimus officia nemo quaerat
-				quia laboriosam quod ex expedita dolores? Itaque eos nam aliquam
-				laborum harum eius veritatis ratione minima fugit. Nulla laborum
-				impedit quae alias quod pariatur sunt recusandae ex? Perferendis
-				corrupti labore, quas illo explicabo ducimus quidem ad minima a
-				laborum voluptatibus? Doloribus porro quod exercitationem
-				voluptatibus saepe quam omnis!
-			</h1>
-			<p>This is joy's conflict</p>
-		</div>
-	);
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad minima eum
+        aut veritatis maxime in iure aliquid quo reiciendis, vel expedita nulla
+        dolor sequi nam autem voluptate rem quisquam laudantium. Corporis
+        tempore placeat doloribus ipsam delectus magni animi perferendis
+        recusandae beatae iste blanditiis eos, molestias natus, suscipit porro
+        ducimus officia nemo quaerat quia laboriosam quod ex expedita dolores?
+        Itaque eos nam aliquam laborum harum eius veritatis ratione minima
+        fugit. Nulla laborum impedit quae alias quod pariatur sunt recusandae
+        ex? Perferendis corrupti labore, quas illo explicabo ducimus quidem ad
+        minima a laborum voluptatibus? Doloribus porro quod exercitationem
+        voluptatibus saepe quam omnis!
+      </h1>
+      <p>This is joy's conflict</p>
+      <p>This is marwa conflict</p>
+    </div>
+  );
 };
 
 export default Home;
